5e2eebcc85fe92c9a67b5f27d315aa95dc5f7a1a
RinatTr <rinatreg@gmail.com>
2022-07-19 12:34:52 -0400

    work in progress

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,4 +1,6 @@
 import React, { useState, useReducer } from 'react';
+import { connect } from 'react-redux';
+//connect() - connects your component to the store. 
 
 import titleReducer, { initialState } from '../reducers/titleReducer';
 import { toggleEditing, updateTitle} from './../actions/titleActions';
@@ -6,8 +8,9 @@ import { toggleEditing, updateTitle} from './../actions/titleActions';
 import TitleDisplay from './TitleDisplay';
 import TitleForm from './TitleForm';
 
-const Title = () => {
-  const [state, dispatch] = useReducer(titleReducer, initialState);
+const Title = (props) => {
+  // const [state, dispatch] = useReducer(titleReducer, initialState);
+  console.log("props from redux??", props)
 
   const handleToggleEditing = () => {
     dispatch(toggleEditing());
@@ -30,4 +33,16 @@ const Title = () => {
   
 };
 
-export default Title;
+const mapStateToProps = (stateFromStore) => {
+  console.log(stateFromStore);
+  return {
+    title: stateFromStore.title
+  }
+}
+//mapStateToProps - will be called every time the store state changes. 
+//it receives the entire store state, and should return an object 
+//of data this component needs. the data will be availble through props.
+
+export default connect(mapStateToProps, {})(Title);
+                //wrapper(Title)
+                //higher order component
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { legacy_createStore } from 'redux'; //redux toolkit
 
 import Title from './components/Title';
 import DragonList from './components/DragonList';
 import './styles.css';
+
+import reducer from './reducers/titleReducer';
+const store = legacy_createStore(reducer);
 
 function App() {
   return (
@@ -15,4 +20,8 @@ function App() {
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, rootElement);
